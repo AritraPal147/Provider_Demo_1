f9520f0f4c9c12f396d4e7c3b713ed0be15c7d89
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-05-25 23:04:02 +0530

    Completed app

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
 import 'dart:collection';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:uuid/uuid.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Flutter Hooks Demo',
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => BreadCrumbProvider(),
+      child: MaterialApp(
+        title: 'Flutter Hooks Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const HomePage(),
+        routes: {
+          '/new': (context) => const NewBreadCrumbWidget(),
+        },
+      ),
     ),
-    debugShowCheckedModeBanner: false,
-    home: const HomePage(),
-  ));
+  );
 }
 
 /// Class that shows the BreadCrumb
@@ -55,7 +64,7 @@ class BreadCrumbProvider extends ChangeNotifier {
   final List<BreadCrumb> _items = [];
 
   /// [UnmodifiableListView] to display items
-  UnmodifiableListView<BreadCrumb> get item => UnmodifiableListView(_items);
+  UnmodifiableListView<BreadCrumb> get items => UnmodifiableListView(_items);
 
   /// Adds a new BreadCrumb to the already existing
   /// list of BreadCrumbs [_items]
@@ -107,6 +116,36 @@ class BreadCrumbsWidget extends StatelessWidget {
   }
 }
 
+/// select() allows us to track specific parts of a provider
+/// instead of the entire provider class
+///
+/// If the aspect of the provider that is tracked by select()
+/// method changes, then, the widget is marked to be rebuilt
+/// Ex:
+/// context.select((value) => aspect1.value);
+///
+/// watch() actually allows us to track changes across the
+/// entire provider
+///
+/// Basically, it will react to any change within the provider
+/// and mark the widget to be rebuilt
+///
+///
+/// The select() and watch() functions can only be used within
+/// build functions. Do not use select and watch functions within
+/// void callbacks.
+///
+///
+/// [Consumer] creates a new widget and calls the  builder with its
+/// own BuildContext. It also wraps itself around the returned
+/// widget from the builder.
+///
+/// It is useful if the widget returned from the builder depends on
+/// the provider before the context itself has access to it.
+///
+/// [Consumer] has a child widget that does not get rebuilt when
+/// the provider changes
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -116,6 +155,99 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          /// [value] is the provider that we pass to the consumer
+          Consumer<BreadCrumbProvider>(
+            builder: (BuildContext context, value, Widget? child) {
+              return BreadCrumbsWidget(
+                breadCrumbs: value.items,
+              );
+            },
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed('/new');
+            },
+            child: const Text(
+              'Add new bread crumb',
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              /// read() gets current snapshot of provider.
+              /// Used for one way communication from callbacks such as onPressed
+              context.read<BreadCrumbProvider>().reset();
+            },
+            child: const Text(
+              'Reset',
+            ),
+          ),
+        ],
+      ),
     );
+  }
+}
+
+class NewBreadCrumbWidget extends StatefulWidget {
+  const NewBreadCrumbWidget({Key? key}) : super(key: key);
+
+  @override
+  State<NewBreadCrumbWidget> createState() => _NewBreadCrumbWidgetState();
+}
+
+class _NewBreadCrumbWidgetState extends State<NewBreadCrumbWidget> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add new bread crumb'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+              hintText: 'Enter new bread crumb here...',
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              final text = _controller.text;
+              /// Checks if TextField is empty or not
+              ///
+              /// If TextField has data, then a new [BreadCrumb] object is
+              /// created and the add() function of the [BreadCrumbProvider]
+              /// is called, and then navigation is done to previous screen.
+              if (text.isNotEmpty) {
+                final breadCrumb =
+                    BreadCrumb(isActive: false, name: _controller.text);
+                context.read<BreadCrumbProvider>().add(breadCrumb);
+                Navigator.of(context).pop();
+              }
+            },
+            child: const Text(
+              'Add',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
