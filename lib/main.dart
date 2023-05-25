84e950878d17dd22cc5c11f893d61f5a602e5674
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-05-25 20:35:04 +0530

    Created BreadCrumb class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -9,6 +10,34 @@ void main() {
     debugShowCheckedModeBanner: false,
     home: const HomePage(),
   ));
+}
+
+/// Class that shows the BreadCrumb
+class BreadCrumb {
+  /// Variable to check if BreadCrumb is active or not
+  bool isActive;
+
+  /// Variable for storing name of BreadCrumb
+  final String name;
+
+  /// Unique uuid for each breadcrumb
+  final String uuid;
+
+  /// Parameterized constructor for initializing all data
+  BreadCrumb({
+    required this.isActive,
+    required this.name,
+  }) : uuid = const Uuid().v4();
+
+  /// Overloading the == operator to compare uuid of 2 breadcrumbs
+  @override
+  bool operator ==(covariant BreadCrumb other) => uuid == other.uuid;
+
+  /// Overloading the hashCode method to get the hashCode of the uuid
+  /// instead of the default hashcode
+  @override
+  int get hashCode => uuid.hashCode;
+
 }
 
 class HomePage extends StatelessWidget {
